a7fa5579d148039fbb3d0a977d571a420cc967d4
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2026-01-27 10:24:34 +0900

    vite react 시작

---
+++ viteapp/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    // 기능은 없고 감싸기 위한 용도 <>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite 기반의 React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+// export 란 외부에서 이 파일을 사용할 수 있도록 허용하되, 외부에서 접근할 이름은 export 뒤에 명시
+// default를 명시하면, 외부에서 이 모듈을 사용할 때 대표이름이 App란 뜻임
+export default App

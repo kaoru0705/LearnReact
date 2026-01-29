86cb82426495d6345808190b5bf8ab26f53d3259
kaoru0705 <51065756+kaoru0705@users.noreply.github.com>
2026-01-29 15:37:15 +0900

    Update App.jsx

--- jwtclient/src/App.jsx
+++ jwtclient/src/App.jsx
@@ -6,6 +6,8 @@ function App() {
   const homepageIdRef = useRef(null);
   const passwordRef = useRef(null);
 
+  const [token, setToken] = useState("");
+
   /*-------------------------------------------
     로그인 요청
   -------------------------------------------*/
@@ -30,6 +32,21 @@ function App() {
     })
     .then(data => {
       console.log("서버에서 가져온 데이터는 ", data);
+      /*----------------------------------------------------------
+      localStorage 란?
+      1) 브라우저가 제공하는 클라이언트 측 영구 저장소
+      2) 저장 데이터 - 문자열 데이터만 약 최대 5MB
+      3) 사용방법
+          저장하기 : localStorage.setItem("fruit", "apple");
+          가져오기 : localStorage.getItem("fruit");
+      4) 쿠키와 차이점
+        쿠키는 무조건 서버로 자동 전송 (storage와 가장 큰 차이점)
+        쿠키는 최대 약 4kb 를 저장
+      
+      5) localStorage는 자바스크립트로 접근이 가능하므로, 보안이 취약..
+      ----------------------------------------------------------*/
+      localStorage.setItem("accessToken", data.accessToken);
+      setToken(data.accessToken);
     })
     .catch(err => console.log(err));
 
@@ -45,14 +62,24 @@ function App() {
 
         <button type="button" style={{border:"1px solid #ddd"}} onClick={login}>로그인</button>
       </form>
+    {/*
+      A && B
+
+      A가 false이면 뒤는 판단할 필요가 없으므로 A 반환
+      A가 true이면 뒤에까지 판단해봐야 하므로 B 반환
+    */}
 
       <div>
-        <div>
-          <b>Access Token</b><br/>
-          발급받은 토큰 나올 예정..
-        </div>
+        {token && (
+          <div>
+            <b>Access Token</b><br/>
+            {token}
+          </div>
+          )
+        }
         <button type="button" style={{border:"1px solid #ddd"}}>로그아웃</button>
       </div>
+
     </div>
   )
 }
